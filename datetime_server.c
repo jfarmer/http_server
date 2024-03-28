cdf31dcd7b2395606781409ef3fa4743413687ba
Jesse Farmer <jesse@20bits.com>
2024-03-28 13:05:52 -0700

    Add more servers

--- datetime_server.c
+++ datetime_server.c
@@ -6,13 +6,29 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <unistd.h>
 
-int main() {	
+
+#define BIND_PORT "3490"
+#define LISTEN_BACKLOG 5
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa) {
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+
+int main() {
   int addr_status, sockfd, bind_status, new_fd, msg_len, bytes_sent;
-	struct addrinfo hints, *res;
+	struct addrinfo hints, *servinfo, *p;;
 	struct sockaddr_storage their_addr;
 	char *msg = "Hi I'm a datetime	";
 	socklen_t addr_size;
+	char s[INET6_ADDRSTRLEN];
 
 	// get local addr
   memset(&hints, 0, sizeof hints);
@@ -20,43 +36,69 @@ int main() {
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags = AI_PASSIVE;
 
-  if ((addr_status = getaddrinfo(NULL, "3490", &hints, &res)) != 0) {
+  if ((addr_status = getaddrinfo(NULL, BIND_PORT, &hints, &servinfo)) != 0) {
       fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(addr_status));
       exit(1);
   }
 	// check for valid results?
-	
-	// get socket
-	if ((sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) !=0) {
-		fprintf(stderr, "socket error: %s\n", gai_strerror(sockfd));
-    exit(1);
-	};
-	
-	// bind to socket
-	if ((bind_status = bind(sockfd, res->ai_addr, res->ai_addrlen)) !=0) {
-		fprintf(stderr, "bind error: %s\n", gai_strerror(bind_status));
-    exit(1);
+
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("server: socket");
+			continue;
+		}
+
+		// if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+		// 		sizeof(int)) == -1) {
+		// 	perror("setsockopt");
+		// 	exit(1);
+		// }
+
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("server: bind");
+			continue;
+		}
+
+		break;
 	}
+
+	freeaddrinfo(servinfo); // all done with this structure
+
+	// // get socket
+	// if ((sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) !=0) {
+	// 	fprintf(stderr, "socket error: %s\n", gai_strerror(sockfd));
+  //   exit(1);
+	// };
+
+	// // bind to socket
+	// if ((bind_status = bind(sockfd, res->ai_addr, res->ai_addrlen)) !=0) {
+	// 	fprintf(stderr, "bind error: %s\n", gai_strerror(bind_status));
+  //   exit(1);
+	// }
 
 	// listen
-	listen(sockfd, 5); //error handling?
+	listen(sockfd, LISTEN_BACKLOG); //error handling?
+	while (1) {
+		// accept
+		addr_size = sizeof their_addr;
+		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
 
-	// accept
-	addr_size = sizeof their_addr;
-	if((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size)) !=0) {
-		fprintf(stderr, "accept error: %s\n", gai_strerror(new_fd));
-    exit(1);
-	}
+		if (new_fd == -1) {
+			fprintf(stderr, "accept error: %s\n", gai_strerror(new_fd));
+			exit(1);
+		}
 
-	msg_len = strlen(msg);
-	bytes_sent = send(sockfd, msg, msg_len, 0);
+		inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
+		printf("server: got connection from %s\n", s);
 
-	if((bytes_sent = send(sockfd, msg, msg_len, 0)) != 0) {
-		fprintf(stderr, "send error: %s\n", gai_strerror(bytes_sent));
-    exit(1);
-	}
+		msg_len = strlen(msg);
+		bytes_sent = send(new_fd, msg, msg_len, 0);
 
-	if(bytes_sent != msg_len) {
-		fprintf(stderr, "failed to send complete message");
+		if(bytes_sent == -1) {
+			perror("Client read failed");
+			exit(1);
+		}
 	}
 }
