8c94eefd07967c0b1a5a66f5917906b9a52d2292
Jesse Farmer <jesse@20bits.com>
2024-03-29 12:54:21 -0700

    [echo_server] Refactor echoing to client into separate function

--- echo_server.c
+++ echo_server.c
@@ -5,6 +5,7 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <errno.h>
 
 #include <unistd.h>
 
@@ -42,7 +43,31 @@ int bind_server_socket(int port) {
   return server_fd;
 }
 
-int main (int argc, char *argv[]) {
+int echo_to_client(int client_fd, void *buffer, size_t buffer_length) {
+  while(1) {
+    int bytes_sent, bytes_recvd;
+
+    bytes_recvd = recv(client_fd, buffer, buffer_length, 0);
+
+    printf("server: read %d bytes\n", bytes_recvd);
+
+    if (bytes_recvd == 0) {
+      // Connection closed in an orderly fashion, so return
+      return 0;
+    }
+
+    if (bytes_recvd < 0) {
+      return bytes_recvd;
+    }
+
+    bytes_sent = send(client_fd, buffer, bytes_recvd, 0);
+    if (bytes_sent < 0) {
+      return bytes_sent;
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
   if (argc < 2) {
     fprintf(stderr, "Usage: %s <port>\n", argv[0]);
     fflush(stderr);
@@ -54,7 +79,6 @@ int main (int argc, char *argv[]) {
   int server_fd, err;
   char net_address[INET6_ADDRSTRLEN];
   char network_buffer[BUFFER_SIZE];
-  struct sockaddr_in server, client;
 
   server_fd = bind_server_socket(port);
   if (server_fd < 0) {
@@ -71,7 +95,9 @@ int main (int argc, char *argv[]) {
   printf("server: listening on port %d\n", port);
 
   while (1) {
+    struct sockaddr_in client;
     socklen_t client_len = sizeof(client);
+
     int client_fd = accept(server_fd, (struct sockaddr *) &client, &client_len);
 
     if (client_fd < 0) {
@@ -82,33 +108,14 @@ int main (int argc, char *argv[]) {
     inet_ntop(client.sin_family, get_in_addr((struct sockaddr *) &client), net_address, sizeof net_address);
 		printf("server: new connection from %s\n", net_address);
 
-    while(1) {
-      int bytes_sent, bytes_recvd;
+    err = echo_to_client(client_fd, network_buffer, BUFFER_SIZE);
+    close(client_fd);
 
-      bytes_recvd = recv(client_fd, network_buffer, BUFFER_SIZE, 0);
-
-      printf("server: read %d bytes\n", bytes_recvd);
-
-      if (bytes_recvd == 0) {
-        // Connection closed in an orderly fashion, so break and wait to accept next connection
-        printf("server: connection closed from %s\n", net_address);
-        close(client_fd);
-        break;
-      }
-
-      if (bytes_recvd < 0) {
-        perror("server: error reading data from network");
-        close(client_fd);
-        break;
-      }
-
-      bytes_sent = send(client_fd, network_buffer, bytes_recvd, 0);
-      if (bytes_sent < 0) {
-        perror("server: error sending data over network");
-        close(client_fd);
-        break;
-      }
-    };
+    if (err < 0) {
+      perror("server: client error");
+    } else {
+      printf("server: connection closed from %s\n", net_address);
+    }
   }
 
   return EXIT_SUCCESS;
